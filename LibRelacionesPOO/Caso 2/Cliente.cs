f016377672d676990340e6b7157483490d1fb1dc
Dyabreu <dayanaabreu2103@gmail.com>
2023-01-13 13:17:33 -0300

    Agregar archivos de proyecto.

---
+++ LibRelacionesPOO/Caso 2/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibRelacionesPOO.Caso_2
+{
+    internal class Cliente
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public List<Pedido> Pedidos { get; set; }
+    }
+}
